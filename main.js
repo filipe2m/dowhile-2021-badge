65846cc0421e43846b1a15affa080e6eff5e2be8
Filipe Mendes <5122694+filipe2m@users.noreply.github.com>
2022-01-14 09:31:56 +0000

    update: minor fixes

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//add your social media usernames to this object
 const LinksSocialMedia = {
   github: '',
   youtube: '',
@@ -12,7 +13,6 @@ function getGitHubProfileInfos() {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      //document.getElementById('userName').textContent = data.nameR
       if (data.login) {
         userAvatar.src = data.avatar_url
         userUrl.href = data.html_url
@@ -26,7 +26,6 @@ function getGitHubProfileInfos() {
 function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
-    //li.children[0].href += linksSocialMedia[social]
     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
   }
 }
